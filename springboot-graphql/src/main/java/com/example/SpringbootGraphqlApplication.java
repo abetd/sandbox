aaf36e0a8cc5eaae9aeb84e04c687b8374703882
abetd <tadanori.abe@gmail.com>
2019-02-04 00:19:29 +0900

    ObjectMapper, Deserializerを追加：変わらず

--- springboot-graphql/src/main/java/com/example/SpringbootGraphqlApplication.java
+++ springboot-graphql/src/main/java/com/example/SpringbootGraphqlApplication.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootGraphqlApplication {
@@ -10,5 +16,12 @@ public class SpringbootGraphqlApplication {
         SpringApplication.run(SpringbootGraphqlApplication.class, args);
     }
 
+    @Bean
+    public ObjectMapper directFieldAccess() {
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                ;
+    }
 }
 
--- springboot-graphql/src/main/java/com/example/presentation/ThreeDaysInput.java
+++ springboot-graphql/src/main/java/com/example/presentation/ThreeDaysInput.java
@@ -1,15 +1,21 @@
 package com.example.presentation;
 
-import com.example.infrastructure.graphql.LocalDateDeserializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class ThreeDaysInput implements Serializable {
+public class ThreeDaysInput {
 
     @JsonDeserialize(using = LocalDateDeserializer.class)
-    LocalDate today;
+//    @JsonFormat(pattern = "yyyy-MM-dd")
+    @JsonProperty
+    public LocalDate today;
 
+    public ThreeDaysInput() {
+        System.out.println("ThreeDaysInput()");
+    }
 }
